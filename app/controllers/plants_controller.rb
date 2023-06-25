34ff2d382154c8bc5bd65c6bddcc4fbf24277af9
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-06-25 17:48:25 +0300

    completed lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+  end
+
+  #DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+    
+  end
+   
+
   private
 
   def plant_params
